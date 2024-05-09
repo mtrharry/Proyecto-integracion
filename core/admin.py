aeedf033b2ed8f219927b9e9cf6ccacef8286f54
mtrharry <89311902+mtrharry@users.noreply.github.com>
2024-05-09 01:22:25 -0400

    configuraciones

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Producto, tipoProducto, stockProducto
 # Register your models here.
+
+admin.site.register(Producto)
+admin.site.register(tipoProducto)
+admin.site.register(stockProducto)
--- core/models.py
+++ core/models.py
@@ -6,11 +6,25 @@ class Producto(models.Model):
     idProducto = models.IntegerField(primary_key=True, verbose_name='Id de Producto')
     nombreProducto = models.CharField(max_length=50, verbose_name='Nombre de Producto')
     precioProducto = models.IntegerField(verbose_name='Precio de Producto')
-    stockProducto = models.IntegerField(verbose_name='Stock de Producto')
+    idTipoProducto = models.ForeignKey('tipoProducto', on_delete=models.PROTECT, verbose_name='Id de Tipo de Producto')
+    
     
 
     def __str__(self):
         return self.nombreProducto
     
     
-
+class tipoProducto(models.Model):
+    idTipoProducto = models.IntegerField(primary_key=True, verbose_name='Id de Tipo de Producto')
+    nombreTipoProducto = models.CharField(max_length=50, verbose_name='Nombre de Tipo de Producto')
+    descripcionTipoProducto = models.CharField(max_length=50, verbose_name='Descripcion de Tipo de Producto')
+    
+    def __str__(self):
+        return self.nombreTipoProducto
+    
+class stockProducto(models.Model):
+    idProducto = models.ForeignKey('Producto', on_delete=models.CASCADE, verbose_name='Id de Producto')
+    stockProducto = models.IntegerField(verbose_name='Stock de Producto')
+    
+    def __str__(self):
+        return self.stockProducto
