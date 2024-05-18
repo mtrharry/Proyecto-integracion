41f5ed519ad7820093fb1f35fa3c30d37060343b
mtrharry <89311902+mtrharry@users.noreply.github.com>
2024-05-18 18:05:54 -0400

    models cascade or protect

--- core/models.py
+++ core/models.py
@@ -7,7 +7,7 @@ from django.dispatch import receiver
 class Producto(models.Model):
     idProducto = models.AutoField(primary_key=True, verbose_name='Id de Producto')
     idTipoProducto = models.ForeignKey('tipoProducto', on_delete=models.PROTECT, verbose_name='Id de Tipo de Producto')
-    cantidadadstock = models.ForeignKey('stockProducto', on_delete=models.PROTECT, verbose_name='Cantidad en Stock', null=True,blank=True)
+    cantidadadstock = models.ForeignKey('stockProducto', on_delete=models.CASCADE , verbose_name='Cantidad en Stock', null=True,blank=True)
 
     def __str__(self):
         return str(self.idTipoProducto.descripcionTipoProducto)
@@ -22,7 +22,7 @@ class tipoProducto(models.Model):
         return self.nombreTipoProducto
     
 class stockProducto(models.Model):
-    idProducto = models.ForeignKey('Producto', on_delete=models.PROTECT, verbose_name='Id de Producto')                        
+    idProducto = models.ForeignKey('Producto', on_delete=models.CASCADE, verbose_name='Id de Producto')                        
     cantidad = models.IntegerField(verbose_name='Stock de Producto', null=True) 
     
     def __str__(self):
